d196ce2119cd949ae5a9a98d6a203037d17ab7d5
Vee-data-analytics <46445235+Vee-data-analytics@users.noreply.github.com>
2023-11-05 00:01:43 +0200

    Update data_detection.py

--- data_detection.py
+++ data_detection.py
@@ -1,37 +1,49 @@
-Automated Data Extraction and Structuring
+import pandas as pd
+import spacy
+import re
 
-This script automates the process of extracting structured data from a text file and organizing it into a tabular format using spaCy for Named Entity Recognition (NER) and pandas for data manipulation. Here's a documentation of what this script does:
-Dependencies
+# Load the spaCy NER model
+nlp = spacy.load("en_core_web_sm")
 
-    pandas for data manipulation.
-    spacy for natural language processing and entity recognition.
-    re for regular expressions.
+# ... (same code to read the file and start_reading)
+file_path = "Pick Place for Yabby3 4G V3.1.txt"
+start_reading = False  # Initialize start_reading
+data = []  # Initialize data as an empty list
 
-Input Data
+with open(file_path, 'r', encoding='latin1') as file:
+    for line in file:
+        # Check if the line contains "Designator" to start reading data
+        if "Designator" in line:
+            start_reading = True
 
-The script starts by reading a text file specified by file_path. It iterates through the lines in the file to extract and structure relevant information.
-Named Entity Recognition (NER)
+        if start_reading:
+            data.append(line.strip())
 
-    It uses spaCy's English NER model (en_core_web_sm) to recognize entities in each line of text.
-    Lines containing "Designator" are used as the starting point for reading data.
-    Recognized entities in each line are stored in recognized_entities.
+# Recognize entities using spaCy NER
+recognized_entities = []
+for line in data:
+    doc = nlp(line)
+    entities = [ent.text for ent in doc.ents]
+    recognized_entities.append(entities)
 
-Data Structuring
+# Organize the recognized entities into structured data
+structured_data = []
+for line, entities in zip(data, recognized_entities):
+    parts = re.split(r'\s{2,}', line)
+    structured_data.append(parts + entities)
 
-    The recognized entities are organized into structured data.
-    The script splits each line into parts based on multiple consecutive whitespace characters (e.g., tabs or spaces) using regular expressions.
-    To ensure a consistent column count in the structured data, empty values are added if necessary.
-    The structured data is stored as a list of lists.
+# Ensure consistent column count
+max_columns = max(len(row) for row in structured_data)
+structured_data = [row + [''] * (max_columns - len(row)) for row in structured_data]
 
-DataFrame Creation
+# Create a DataFrame
+df = pd.DataFrame(structured_data, columns=[f"Col_{i}" for i in range(max_columns)])
 
-    A pandas DataFrame is created using the structured data, with columns named Col_0, Col_2, Col_4, Col_5, and Col_6.
-    These columns correspond to "Designator," "Layer," "Center-X," "Center-Y," and "Rotation."
-    Rows containing "Comment" in the "Designator" column are filtered out from the DataFrame.
-    The resulting DataFrame is saved as an Excel file named "excel_pinck_.xlsx."
+df = df[['Col_0', 'Col_2', 'Col_4', 'Col_5', 'Col_6']]
+df.columns = ['Designator', 'Layer', 'Center-X', 'Center-Y', 'Rotation']
+df = df[~df['Designator'].str.contains('Comment')]
+# Print the modified DataFrame
 
-Output
+df.to_excel('excel_pinck_.xlsx', index=False)
 
-The script prints the modified DataFrame, which contains the extracted and structured data, excluding rows with "Comment" in the "Designator" column.
-
-This script is useful for automatically extracting structured data from text files, such as engineering or manufacturing reports, and converting it into a more accessible and analyzable format. It's a practical solution for tasks that involve processing unstructured text data into structured tables for further analysis.
+print(df)
